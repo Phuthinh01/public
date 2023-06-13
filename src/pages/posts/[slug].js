0da303bbbfb025275e09996e996960b19d6a5064
Phuthinh01 <105631817+Phuthinh01@users.noreply.github.com>
2023-06-13 18:12:54 +0700

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -74,19 +74,15 @@ export default function Post({ post, socialImage, related }) {
 
       <Header>
         <meta property="og:title" content={post.title} />
-				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:url" content={`https://${host}/${path}`} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-        )}
+		<link rel="canonical" href={`https://${host}/${path}`} />
+		<meta property="og:description" content={removeTags(post.excerpt)} />
+		<meta property="og:url" content={`https://${host}/${path}`} />
+		<meta property="og:type" content="article" />
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:site_name" content={host.split('.')[0]} />
+		<meta property="article:published_time" content={post.dateGmt} />
+		<meta property="article:modified_time" content={post.modifiedGmt} />
+		<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
         <h1
           className={styles.title}
           dangerouslySetInnerHTML={{
